88c1564b1d2fb1c852502062548d6e76c7013d0c
Morita Tomoki <morita_tomoki@icloud.com>
2023-11-19 21:52:59 +0900

    リファクタリング

--- Propella-Thrust-Test.ino
+++ Propella-Thrust-Test.ino
@@ -13,8 +13,10 @@
 #define ADC_VREF     5000    // 5V Vref
 #define ADC_CLK      1600000 // SPI clock 1.6MHz
 #define ESC_PIN      9       // ESCのピン番号
+#define SERVO_PIN    3       // ダクト偏向用サーボのピン番号
 #define PULSE_INPUT  2       // RPM計測用の入力ピン
-#define ADC_INPUT    A1      // ADC用のアナログ入力ピン
+#define ADC_INPUT_ESC    A1      // ESC操作用のアナログ入力ピン
+#define ADC_INPUT_SERVO  A0      // サーボ操作用のアナログ入力ピン
 
 // INA238のI2Cアドレスを指定する
 const uint8_t INA238_ADDRESS1 = 0x40; // デバイス1のアドレス
@@ -24,37 +26,55 @@ INA238 powerMonitor(INA238_ADDRESS1);
 
 MCP3204 adc(ADC_VREF, SPI_CS);
 Servo esc;
+Servo duct;
 volatile unsigned long lastPulseTime = 0;
 volatile unsigned long prevPulseTime = 0;
 volatile bool newPulse = false;
 unsigned long lastReportTime = 0;
 const float pulsesPerRevolution = 1.0;
-
+int pwmValueESC = 1000;
+int pwmValueServo = 1000;
 
 void setup() {
-  // シリアル通信を開始
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // シリアルポートが利用可能になるまで待機
-  }
-  Serial.println("プログラム開始");
+  initializeSerial();
+  initializeINA238();
+  initializeSPI();
+  initializeRPMMeasurement();
+  initializeESC();
+  initializeServo();
+}
 
-  // INA238を開始し、通信ができるか確認
+void loop() {
+  manageESC();
+  manageServo();
+
+  reportINA238Data();
+  readLoadcell();
+  readMotorRPM();
+  reportESC_Signal();
+  reportServoSignal();
+  Serial.println("-----------------------");
+
+  delay(1000); // 1秒間隔でデータを読み取る
+}
+
+void initializeSerial() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Start Program");
+}
+
+void initializeINA238() {
   if (!powerMonitor.begin()) {
     Serial.println("INA238が見つかりません。配線を確認してください。");
     while (1); // エラーが発生した場合は無限ループ
   }
-
-  // INA238を設定
   if (!powerMonitor.configure()) {
     Serial.println("INA238の設定に失敗しました。");
   }
-
-  // ADCを設定
   if (!powerMonitor.adcConfigure()) {
     Serial.println("INA238のADC設定に失敗しました。");
   }
-
   Serial.println("INA238 ADC設定完了");
 
   uint16_t shuntCalibrationData;
@@ -67,25 +87,36 @@ void setup() {
     // エラーが発生したため、シリアルポート経由でエラーメッセージを出力する
     Serial.println("Error reading shunt calibration data.");
   }
+}
 
-  // ロードセルADC用のSPIピン設定
+void initializeSPI() {  // ロードセルADC用のSPIピン設定
   pinMode(SPI_CS, OUTPUT);
   digitalWrite(SPI_CS, HIGH);
   SPISettings settings(ADC_CLK, MSBFIRST, SPI_MODE0);
   SPI.begin();
-  SPI.beginTransaction(settings);
+  SPI.beginTransaction(settings); 
+}
 
+void initializeRPMMeasurement() {
   // RPM計測用のピン設定
   pinMode(PULSE_INPUT, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(PULSE_INPUT), pulseDetected, FALLING);
+}
 
+void initializeESC() {
   // ESCの設定
   esc.attach(ESC_PIN);
   esc.writeMicroseconds(1000); // ESCをアーム
   delay(5000); // スタートアップのための待ち時間
 }
 
-void loop() {
+void initializeServo() {
+  // ESCの設定
+  duct.attach(SERVO_PIN);
+  //duct.writeMicroseconds(1000);
+}
+
+void reportINA238Data() {
   // INA238からデータを読み取る
   float current = powerMonitor.readCurrent(); // 電流を読み取る
   float shunt = powerMonitor.readShuntVoltage(); // シャント電圧を読み取る
@@ -112,26 +143,10 @@ void loop() {
 
   Serial.print("Temperature: ");
   Serial.print(temperature);
-  Serial.println(" C");
-
-  // ADC値の読み取り
-  readADC();
-
-  // RPMの計算とPWM値の出力
-  manageRPMandPWM();
-  
-  Serial.println("-----------------------");
-
-  delay(1000); // 1秒間隔でデータを読み取る
+  Serial.println(" C");  
 }
 
-void manageRPMandPWM() {
-  // PWM値の計算
-  int sensorValue = analogRead(ADC_INPUT);
-  int pwmValue = map(sensorValue, 0, 1023, 1000, 2000);
-  esc.writeMicroseconds(pwmValue);
-
-  // RPMの計算
+void readMotorRPM() {
   float rpm = 0;
   if (newPulse) {
     unsigned long timeDifference = lastPulseTime - prevPulseTime;
@@ -139,18 +154,35 @@ void manageRPMandPWM() {
       rpm = (60000000.0 / timeDifference) / pulsesPerRevolution;
     }
     newPulse = false;
-  }
-
-  // PWM値とRPMの出力
-  Serial.print("PWM: ");
-  Serial.print(pwmValue);
-  Serial.println(" us");
-  
+  }  
   Serial.print("RPM: ");
   Serial.println(rpm);
 }
 
-void readADC() {
+void manageESC() {
+  int sensorValue = analogRead(ADC_INPUT_ESC);
+  pwmValueESC = map(sensorValue, 0, 1023, 1000, 2000);
+  esc.writeMicroseconds(pwmValueESC);
+}
+
+void reportESC_Signal() {
+  Serial.print("ESC_Signal: ");
+  Serial.print(pwmValueESC);
+  Serial.println(" us");
+}
+
+void reportServoSignal() {
+  Serial.print("ServoSignal: ");
+  Serial.print(pwmValueServo);
+  Serial.println(" us");
+}
+
+void manageServo() {
+  int sensorValue = analogRead(ADC_INPUT_SERVO);
+  pwmValueServo = map(sensorValue, 0, 1023, 1000, 2000);
+  duct.writeMicroseconds(pwmValueServo);
+}
+void readLoadcell() {
   uint32_t t1 = micros();
   uint16_t raw = adc.read(MCP3204::Channel::SINGLE_0);
   uint32_t t2 = micros();
@@ -164,9 +196,9 @@ void readADC() {
   Serial.println(" mV)");
 
   // サンプリング時間の出力
-  Serial.print("Sampling time: ");
-  Serial.print(static_cast<double>(t2 - t1) / 1000, 4);
-  Serial.println(" ms");
+  //Serial.print("Sampling time: ");
+  //Serial.print(static_cast<double>(t2 - t1) / 1000, 4);
+  //Serial.println(" ms");
 }
 
 void pulseDetected() {
